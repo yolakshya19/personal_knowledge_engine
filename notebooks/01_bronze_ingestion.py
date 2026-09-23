396db993611009e1d5466502891d01df5163d178
Lakshya Dhawan <lakshyad1902@gmail.com>
2026-09-23 09:42:16 +0530

    databricks update

--- notebooks/01_bronze_ingestion.py
+++ notebooks/01_bronze_ingestion.py
@@ -1,8 +1,5 @@
 # Databricks notebook source
-
-# Bronze ingestion pipeline
-
-# CELL 1
+# CELL 1 - imports
 import requests
 import json
 from datetime import datetime, timezone
@@ -17,6 +14,9 @@ from pyspark.sql.types import (
 )
 from dotenv import load_dotenv
 
+# COMMAND ----------
+
+# CELL 2 - load env
 load_dotenv("configs/.env")
 
 key = os.getenv("JOOBLE_API_KEY")
@@ -25,23 +25,21 @@ url = f"https://jooble.org/api/{key}"
 
 # COMMAND ----------
 
-# CELL 2
-payload = {"keywords": "Software Engineer", "location": "India"}
+payload = {"location": "India"}
 
 response = requests.post(url, json=payload)
 
 print("Status:", response.status_code)
-# print(response.text)
-
 data = response.json()
+print(json.dumps(data, indent= 2, ensure_ascii= False))
 
-print("Total jobs:", data["totalCount"])
+# print("Total jobs:", data["totalCount"])
 
-for job in data["jobs"]:
-    print(f"\nTitle: {job['title']}")
-    print(f"Company: {job.get('company')}")
-    print(f"Location: {job.get('location')}")
-    print(f"Updated: {job.get('updated')}")
-    print(f"URL: {job.get('link')}")
+# for job in data["jobs"]:
+#     print(f"\nTitle: {job['title']}")
+#     print(f"Company: {job.get('company')}")
+#     print(f"Location: {job.get('location')}")
+#     print(f"Updated: {job.get('updated')}")
+#     print(f"URL: {job.get('link')}")
 
 # COMMAND ----------
